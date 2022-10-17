883d228ea96323521b5532f2156679c1f34273a9
MSutton123 <106132991+MSutton123@users.noreply.github.com>
2022-10-17 11:07:42 +1030

    Add files via upload

---
+++ ws11.cpp
@@ -0,0 +1,30 @@
+// This is a file that is meant to contain some code
+
+// but my group could not figure it out
+
+// because we had to work on the project
+
+// so we have this instead
+
+// enjoy
+
+#include <iostream>
+
+int main() {
+    std::cout << "Un means 1 in French" << std::endl;
+    std::cout << "Deux means 2 in French" << std::endl;
+    std::cout << "Trois means 3 in French" << std::endl;
+    std::cout << "Quatre means 4 in French" << std::endl;
+    std::cout << "Cinq means 5 in French" << std::endl;
+    std::cout << "Six means 6 in French" << std::endl;
+    std::cout << "Sept means 7 in French" << std::endl;
+    std::cout << "Huit means 8 in French" << std::endl;
+    std::cout << "Neuf means 9 in French" << std::endl;
+    std::cout << "Dix means 10 in French" << std::endl;
+
+    std::cout << " " << std::endl;
+    std::cout << " " << std::endl;
+
+    std::cout << "merci!"<< std::endl;
+    std::cout << "*_*" << std::endl;
+}
